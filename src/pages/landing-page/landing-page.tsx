e9720527b9e535d1439ff214f5a9ce470d7d84ae
hungpt2004 <141483872+hungpt2004@users.noreply.github.com>
2025-05-28 08:09:33 +0700

    feat(ui): fix notification

--- src/pages/landing-page/landing-page.tsx
+++ src/pages/landing-page/landing-page.tsx
@@ -1,12 +1,10 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
-  Search,
   ChevronRight,
   Star,
   BookOpen,
   Users,
   GraduationCap,
-  Divide,
 } from "lucide-react";
 import {
   Carousel,
